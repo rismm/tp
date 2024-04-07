6a3bcdf15a3a167b29575ad4c09d4a1710a28711
vimalapugazhan <p.vimalapugazhan@gmail.com>
2024-04-07 23:29:51 +0800

    Edit printing for printRevenueExpenditure to print money to 2dp

--- src/main/java/supertracker/ui/Ui.java
+++ src/main/java/supertracker/ui/Ui.java
@@ -223,33 +223,32 @@ public class Ui {
         }
     }
 
-    //@@author vimalapugazhan
     public static void printRevenueExpenditure(String task, double amount, LocalDate startDate, LocalDate endDate,
                                                String financeType, ArrayList<Transaction> filteredList) {
         amount = roundTo2Dp(amount);
+        String amountString = String.format("%.2f", amount);
         switch (task) {
         case TODAY:
-            printIndent("Today's " + financeType + " is $" + amount);
+            printIndent("Today's " + financeType + " is $" + amountString);
             printFilteredList(filteredList);
             break;
         case TOTAL:
-            printIndent("Total  " + financeType + "  is $" + amount);
+            printIndent("Total  " + financeType + "  is $" + amountString);
             printFilteredList(filteredList);
             break;
         case DAY:
-            printIndent(financeType + " on " + startDate.format(DATE_FORMAT_PRINT) + " was $" + amount);
+            printIndent(financeType + " on " + startDate.format(DATE_FORMAT_PRINT) + " was $" + amountString);
             printFilteredList(filteredList);
             break;
         case RANGE:
             printIndent( financeType + " between " + startDate.format(DATE_FORMAT_PRINT) + " and "
-                    + endDate.format(DATE_FORMAT_PRINT) + " was $" + amount);
+                    + endDate.format(DATE_FORMAT_PRINT) + " was $" + amountString);
             printFilteredList(filteredList);
             break;
         default: assert task.isEmpty();
             break;
         }
     }
-    //@@author
 
     // author dtaywd
     private static void printFilteredList(ArrayList<Transaction> filteredList) {
