137a94564da5fb6f08cff087ea1f4642d478e869
awesomesjh <simjunhong@gmail.com>
2024-04-09 21:41:47 +0800

    Fix code indentation

--- src/main/java/supertracker/ui/HelpCommandUi.java
+++ src/main/java/supertracker/ui/HelpCommandUi.java
@@ -6,17 +6,17 @@ public class HelpCommandUi extends Ui {
     private static final String HELP_SUCCESS_MESSAGE_SECOND_LINE =
             "** Any other invalid input will bring you back to the main console";
     private static final String[] HELP_LIST_OF_COMMANDS = {
-        "Create a new item: type 'new' to show parameters",
-        "Delete an item: type 'delete' to show parameters",
-        "Change quantity of an item: type 'change' to show parameters",
-        "Update an item: type 'update' to show parameters",
-        "Find an item: type 'find' to show parameters",
-        "Rename an item: type 'rename' to show parameters",
-        "List all items: type 'list' to show parameters",
-        "Print a report: type 'report' to show parameters",
-        "Print expenditure: type 'exp' to show parameters",
-        "Print revenue: type 'rev' to show parameters",
-        "Buy or sell items: type 'transaction' to show parameters"
+            "Create a new item: type 'new' to show parameters",
+            "Delete an item: type 'delete' to show parameters",
+            "Change quantity of an item: type 'change' to show parameters",
+            "Update an item: type 'update' to show parameters",
+            "Find an item: type 'find' to show parameters",
+            "Rename an item: type 'rename' to show parameters",
+            "List all items: type 'list' to show parameters",
+            "Print a report: type 'report' to show parameters",
+            "Print expenditure: type 'exp' to show parameters",
+            "Print revenue: type 'rev' to show parameters",
+            "Buy or sell items: type 'transaction' to show parameters"
     };
     private static final String HELP_CLOSING_MESSAGE_FIRST_LINE =
             "** Refer to UserGuide for further explanation";
